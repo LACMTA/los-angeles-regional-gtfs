c15355cc58c0c92ae757df625647d83525a688e2
Nina Kin <matikin9@gmail.com>
2021-12-07 10:23:04 -0800

    initial files with updated feed_info.txt

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # los-angeles-regional-gtfs
-LA Metro is hosting GTFS data on behalf of various regional agencies
+
+Metro is hosting GTFS data on behalf of various regional agencies. Each folder contains the `*.txt` files and a `.zip` with all the files bundled together.  These files were originally put together by Trillium and then transferred to Metro.
+
+## Disclaimer
+
+While we will do our best to ensure the data is kept accurate and up to date, we cannot be held liable for any omissions and inaccuracies.
---
+++ replace_trillium.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import os
+import fileinput
+import re
+
+# folders = os.listdir()
+# print(folders)
+
+def replace_Trillium(text):
+	text = text.replace("http://www.trilliumtransit.com", "https://www.metro.net")
+	text = text.replace("Trillium Solutions, Inc.", "Los Angeles County Metropolitan Transportation Authority")
+	text = text.replace("http://support.trilliumtransit.com", "https://developer.metro.net")
+	text = re.sub(r'support.*@trilliumtransit\.com', 'csinteractive@metro.net', text)
+	return text
+
+thisdir = os.getcwd()
+
+for r, d, f in os.walk(thisdir):
+	for file in f:
+		if file.endswith("feed_info.txt"):
+			print(os.path.join(r, file))
+			with open(os.path.join(r, file), 'r') as f:
+				filedata = f.read()
+			filedata = replace_Trillium(filedata)
+			with open(os.path.join(r, file), 'w') as f:
+				f.write(filedata)
+
+# for f in folders:
+# 	if not os.path.isfile(f):
+# 		thisdir = os.listdir(f)
+# 		for file in thisdir:
+# 			if file.endswith("feed_info.txt"):
+# 				print(os.path.join(f, file))
+
+
