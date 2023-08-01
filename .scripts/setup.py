2143f8a5db67766a60c18a4b793d68c4be99ab6c
albertkun <albertk@gmx.com>
2023-08-01 00:53:47 -0700

    fix: setup.py file location

--- .scripts/setup.py
+++ .scripts/setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules=cythonize("gtfs_static_utils.pyx",
+    ext_modules=cythonize(r"./.scripts/gtfs_static_utils.pyx",
     compiler_directives = { "language_level" : "3str"}),
 )
