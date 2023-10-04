7ea3126dd089156f2963f7bcff9c6856d4244b5b
albertkun <albertk@gmx.com>
2023-10-03 19:45:09 -0700

    fix:  `stops_df` was not referenced

--- .scripts/gtfs_static_updater.py
+++ .scripts/gtfs_static_updater.py
@@ -197,7 +197,7 @@ def update_gtfs_static_files():
         print("******************")
     join_trips_to_stop_times = pd.merge(trips_df, stop_times_df, on='trip_id', how='inner')
     simplified_trips_join_stop_times = join_trips_to_stop_times[['trip_id','route_id','direction_id','shape_id','stop_id', 'stop_sequence','route_code', 'pickup_type', 'drop_off_type']]
-    simplified_trips_join_stop_times_join_to_stops = pd.merge(simplified_trips_join_stop_times, gtfs_stops, on='stop_id', how='inner')
+    simplified_trips_join_stop_times_join_to_stops = pd.merge(simplified_trips_join_stop_times, stops_df, on='stop_id', how='inner')
     simplified_trips_join_stop_times_join_to_stops = simplified_trips_join_stop_times_join_to_stops[['trip_id','route_id','direction_id','shape_id','stop_id', 'stop_sequence','route_code', 'pickup_type', 'drop_off_type', 'stop_name']]
     simplified_trips_join_stop_times_join_to_stops
 
